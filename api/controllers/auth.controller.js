82e4dc06cece6060f6b27f2156de323d2f5990bb
Rishipreeth <rpoket47@gmail.com>
2024-08-07 20:59:44 +0530

    Added files

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+export const register=(req,res)=>{
+    console.log("register endpoint");
+}
+
+export const login=(req,res)=>{
+    //db operations
+}
+
+
+export const logout=(req,res)=>{
+    //db operations
+}
+
