c847292ce0016634c0290e22bbeadc43fcabbb90
akashsiripuram <144911464+akashsiripuram@users.noreply.github.com>
2024-08-12 00:31:48 +0530

    client side updated

---
+++ Client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import "./Navbar.scss"
+export default function Navbar(){
+    const [open,setOpen]=useState(false);
+    return(
+        <nav>
+            <div className="left">
+                <a href="/" className="logo">
+                    <img src="/logo.png" alt="" />
+                    <span>Real Estate</span>
+                </a>
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Contact</a>
+                <a href="/">Agents</a>
+            </div>
+            <div className="right">
+                <a href="/" >SignIn</a>
+                <a href="/" className="register">SignUp</a>
+                <div className="menuIcon">
+                    <img src="menu.png" alt="" onClick={()=>setOpen(!open)} />
+                </div>
+                <div className={open?"menu active":"menu"}>
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Contact</a>
+                <a href="/">Agents</a>
+                <a href="/">Sign In</a>
+                <a href="/">Sign Up</a>
+                </div>
+            </div>
+        </nav>
+    )
+
+}
---
+++ Client/src/routes/homePage/HomePage.jsx
@@ -0,0 +1,18 @@
+// @flow 
+import "./HomePage.scss";
+
+export default function HomePage(){
+    return (
+       <div className="homePage">
+        <div className="textContainer">
+            <div className="wrapper">
+            <h1>Find Real Estate & Get Your Dream Place</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt vitae labore atque nesciunt, explicabo in eos at expedita id cum non similique quos pariatur ex repellendus, quam, aliquam quia eligendi?</p>
+            </div>
+        </div>
+        <div className="imgContainer">
+            <img src="./bg.png" alt="" />
+        </div>
+       </div>
+    );
+}
