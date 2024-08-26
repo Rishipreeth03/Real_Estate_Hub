c69fee6eca9662dafb9272fd21f0c3a563c68e80
akashsiripuram <144911464+akashsiripuram@users.noreply.github.com>
2024-08-27 01:04:05 +0530

    updated homepage

--- Client/src/routes/homePage/HomePage.jsx
+++ Client/src/routes/homePage/HomePage.jsx
@@ -1,5 +1,6 @@
 // @flow 
 import "./HomePage.scss";
+import SearchBar from "../../components/searchbar/SearchBar"; "../../components/searchbar/SearchBar";
 
 export default function HomePage(){
     return (
@@ -8,6 +9,21 @@ export default function HomePage(){
             <div className="wrapper">
             <h1>Find Real Estate & Get Your Dream Place</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt vitae labore atque nesciunt, explicabo in eos at expedita id cum non similique quos pariatur ex repellendus, quam, aliquam quia eligendi?</p>
+            <SearchBar/>
+            <div className="boxes">
+                <div className="box">
+                    <h1>16+</h1>
+                    <h2>Years of Experience</h2>
+                </div>
+                <div className="box">
+                    <h1>200</h1>
+                    <h2>Award Gained</h2>
+                </div>
+                <div className="box">
+                    <h1>1200+</h1>
+                    <h2>Property Ready</h2>
+                </div>
+            </div>
             </div>
         </div>
         <div className="imgContainer">
